6fb7f6ecc187430a922dde6d97601e9136d33bf6
Svyatocheck <svyatwork3@yahoo.com>
2023-05-15 17:50:24 +0300

    fixes

--- prepare_audio.py
+++ prepare_audio.py
@@ -4,65 +4,66 @@ from constants import *
 
 class FeatureInputGenerator:
 
-    def start_preprocess(self, audio, isVideo):
+    def start_preprocess(self, audio):
         try:
-            spectrogram = self._make_spectrograms(audio, isVideo)
-        except:
-            raise Exception('Audio is empty, we can not help ya!')
-        
-        self._put_phase(spectrogram)    
+            spectrogram = self._make_spectrograms(audio)
+        except Exception as ex:
+            print(f"There is problem with file: {ex}")
+            return
+
+        self._put_phase(spectrogram)
         spectrogram = self._calculate_means(spectrogram)
-        
+
         X = self._reshape_predictors(self._prepare_input_features(spectrogram))
         x_predictor = np.asarray(X).astype(np.float32)
         return x_predictor
-    
-    
-    def _read_audio_files(self, path, normalize, isVideo = False):
-        '''Загрузка, удаление тихих участков из аудио файла, нормализация.'''
-        audio, _ = librosa.load(path, sr=SAMPLE_RATE)
+
+
+    def _make_spectrograms(self, audio_path, normalize=False):
+        """
+        This function takes in an audio file and generates spectrograms using the librosa library.
+        :param audio_path: path to audio file
+        :param normalize: whether or not to normalize the spectrograms
+        :return: spectrogram
+        """
+        audio_np, _ = librosa.load(audio_path, sr=SAMPLE_RATE)
         if normalize:
-            if isVideo:
-                audio, _ = librosa.effects.trim(audio)
-            div_fac = 1 / np.max(np.abs(audio)) / 3.0
-            audio = audio * div_fac
-        return audio
-    
-    
-    def _make_spectrograms(self, audio, clean = False, isVideo = False):
-        '''Создание STFT диаграмм.'''
-        audio = self._read_audio_files(audio, clean, isVideo)
-        stft = librosa.stft(y=audio, n_fft=N_FFT,hop_length=OVERLAP, center=True, window='hamming')
-        return stft
-    
-    
+            div_fac = 1 / np.max(np.abs(audio_np)) / 3.0
+            audio_np = audio_np * div_fac
+        return librosa.stft(y=audio_np, hop_length=OVERLAP, n_fft=N_FFT, center=True, window='hamming')
+
+
     def _calculate_means(self, spectrogram):
-        '''Необходимый в предобработке звукового сигнала этап.
-        Взят из статьи 1609.'''
+        '''Preprocessing step for sound signal. 
+        Taken from article 1609'''
         stft_feature = np.abs(spectrogram)
         mean = np.mean(stft_feature)
         std = np.std(stft_feature)
         self.mean = mean
         self.std = std
         return (stft_feature - mean) / std
-    
-    
+
+
     def _prepare_input_features(self, items):
         '''Формирование векторов из STFT диаграмм.'''
-        stft_feature = np.concatenate([items[:,0:N_SEGMENTS-1], items], axis=1)
-        stft_segments = np.zeros((N_FEATURES, N_SEGMENTS , stft_feature.shape[1] - N_SEGMENTS + 1))
+        stft_feature = np.concatenate(
+            [items[:, 0:N_SEGMENTS-1], items], axis=1)
+        stft_segments = np.zeros(
+            (N_FEATURES, N_SEGMENTS, stft_feature.shape[1] - N_SEGMENTS + 1))
         for index in range(stft_feature.shape[1] - N_SEGMENTS + 1):
-            stft_segments[:,:,index] = stft_feature[:,index:index + N_SEGMENTS]
+            stft_segments[:, :, index] = stft_feature[:,
+                                                      index:index + N_SEGMENTS]
         return stft_segments
-    
-    
+
+
     def _reshape_predictors(self, items):
         '''Решейп векторов в требуемый НС формат.'''
         predictors = np.reshape(items, (items.shape[0], items.shape[1], 1, items.shape[2]))
         predictors = np.transpose(predictors, (3, 0, 1, 2)).astype(np.float32)
         print(predictors.shape)
         return predictors
-    
-    
+
+
     def _put_phase(self, spectrogram):
         self.audio_phase = np.angle(spectrogram)
+
